c1b2242a092fcd2fb82df9cd7adbcaff7970597f
Sai Lalith <sailalith192@gmail.com>
2026-02-11 19:57:20 +0530

    first commit

---
+++ theReceiptPrint.py
@@ -0,0 +1,5 @@
+item = input("what is the name of the product? ")
+price = int(input("what is the price? "))
+Quantity = int(input("How much quantity,Do you want? "))
+Total = price * Quantity
+print(Total)
